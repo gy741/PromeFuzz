5c69ff5e70df526d5748aed9a6c2ce30131cd865
gy741 <gy741.kim@gmail.com>
2026-06-26 23:03:34 +0900

    Create build.sh

---
+++ database/dlt-daemon/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+. ../../common.sh $1
+
+echo "start compiling $PWD with $MODE"
+
+rm -rf build_$MODE bin_$MODE
+mkdir build_$MODE
+pushd build_$MODE
+
+cmake ../code \
+    -DCMAKE_INSTALL_PREFIX=$PWD/../bin_$MODE \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_C_FLAGS_DEBUG=" -w " \
+    -DCMAKE_CXX_FLAGS_DEBUG=" -w " \
+    -DWITH_DOC=ON \
+    -DWITH_MAN=ON \
+    -DDLT_IPC=UNIX_SOCKET \
+    -DDLT_USER_IPC_PATH=/ipc \
+    -DWITH_DLT_DEBUGGERS=OFF \
+    -DWITH_DLT_SYSTEM=OFF \
+    -DWITH_DLT_TESTS=ON \
+    -DWITH_DLT_USE_IPv6=OFF \
+    -DWITH_EXTENDED_FILTERING=OFF \
+    -DWITH_SYSTEMD=OFF \
+    -DWITH_SYSTEMD_WATCHDOG=OFF \
+    -DWITH_SYSTEMD_JOURNAL=OFF
+
+if [[ $MODE == "asan" ]]; then
+    bear -- make -j$JOBS || exit 1
+else
+    make -j$JOBS || exit 1
+fi
+
+make install || exit 1
+
+popd
+
+echo "end compiling $PWD with $MODE"
