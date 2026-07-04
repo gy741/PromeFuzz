146573cad05841554f9e494f54f0311d3117352b
gy741 <gy741.kim@gmail.com>
2026-07-04 10:41:14 +0900

    Update build.sh

--- database/dlt-daemon/build.sh
+++ database/dlt-daemon/build.sh
@@ -14,14 +14,9 @@ cmake ../code \
     -DCMAKE_CXX_FLAGS="-w -Wno-unknown-warning-option" \
     -DCMAKE_C_FLAGS_DEBUG=" -w -Wno-unknown-warning-option" \
     -DCMAKE_CXX_FLAGS_DEBUG=" -w -Wno-unknown-warning-option" \
-    -DWITH_DOC=ON \
     -DDLT_IPC=UNIX_SOCKET \
-    -DDLT_USER_IPC_PATH=/ipc \
-    -DWITH_DLT_DEBUGGERS=OFF \
     -DWITH_DLT_SYSTEM=OFF \
-    -DWITH_DLT_TESTS=ON \
-    -DWITH_DLT_USE_IPv6=OFF \
-    -DWITH_EXTENDED_FILTERING=OFF \
+    -DWITH_DLT_DEBUGGERS=OFF \
     -DWITH_SYSTEMD=OFF \
     -DWITH_SYSTEMD_WATCHDOG=OFF \
     -DWITH_SYSTEMD_JOURNAL=OFF
