6be273a5dbbbca37a7fa4e9a18cc51cba7b448cc
gy741 <gy741.kim@gmail.com>
2026-07-04 10:33:37 +0900

    Update install_deps.sh

--- database/dlt-daemon/install_deps.sh
+++ database/dlt-daemon/install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPS="cmake zlib1g-dev libdbus-glib-1-dev build-essential doxygen graphviz libjson-c-dev asciidoc pandoc"
+DEPS="cmake zlib1g-dev libdbus-glib-1-dev build-essential libjson-c-dev"
 if [ "$EUID" -eq 0 ]; then
     apt install $DEPS
 else
