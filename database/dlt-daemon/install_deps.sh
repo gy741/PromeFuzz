f6def91d7a2d12af619e807d146f6b2713bc14bc
gy741 <gy741.kim@gmail.com>
2026-07-10 20:24:36 +0900

    Update install_deps.sh

--- database/dlt-daemon/install_deps.sh
+++ database/dlt-daemon/install_deps.sh
@@ -4,5 +4,5 @@ DEPS="cmake zlib1g-dev libdbus-glib-1-dev build-essential libjson-c-dev"
 if [ "$EUID" -eq 0 ]; then
     apt install $DEPS
 else
-    apt install $DEPS
+    sudo apt install $DEPS
 fi
